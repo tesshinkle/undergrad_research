05192a82f9b346d63bdb6288546627df5e714278
tesshinkle <isabellajudehinkle@gmail.com>
2026-08-29 14:33:29 -0500

    Attempt at trying to get the indycarpy package to work, was unsuccessful

---
+++ indycar_data.py
@@ -0,0 +1,10 @@
+#pretty sure package was not created correctly
+#for Terminal: pip install indycarpy
+#for terminal: pip install pandas requests tqdm numpy
+
+
+#import sys
+#print(sys.executable)
+
+
+#import indycarpy
